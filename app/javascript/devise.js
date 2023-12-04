2de884c7d1ea826a1470fe5a3dc265591e256563
Douglas Marins <dmv@tokenizadora.com.br>
2023-12-04 19:14:29 -0300

    implemantação da mascara de telefone

--- app/javascript/devise.js
+++ app/javascript/devise.js
@@ -1,1 +1,14 @@
+import 'jquery';
 import "./shared-config";
+import Inputmask from "inputmask";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const phoneInput = document.querySelector(".js-mask-phone");
+
+  if (phoneInput) {
+    $(document).ready(function() {
+      const im_phone = new Inputmask("99 99999-9999");
+      im_phone.mask(phoneInput);
+    });
+  }
+});
