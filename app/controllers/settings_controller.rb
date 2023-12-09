e68e65ffc5630d167403cc303b5f2770b14d6618
Douglas Marins <dmv@tokenizadora.com.br>
2023-12-09 18:49:05 -0300

    atualização e criação para alteração de senha

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -16,9 +16,31 @@ class SettingsController < ApplicationController
     end
   end
 
+  def update_password
+    if !current_password_params[:current_password].blank? && current_user.valid_password?(current_password_params[:current_password])
+      if current_user.update(password_params)
+        bypass_sign_in(current_user)
+        redirect_to settings_path, flash[:success] = "Senha atualizada com sucesso"
+      else
+        render :password
+      end
+    else
+      flash[:alert] = "Digite sua senha atual"
+      redirect_to password_settings_path
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :phone, :avatar)
+  end
+
+  def current_password_params
+    params.require(:user).permit(:current_password)
+  end
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
   end
 end
